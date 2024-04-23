a6111be4483548e22bc835c08c51c3fc2fea0783
tsegaywelu <tsegaygebrezgi56@gmail.com>
2024-04-23 23:28:20 +0300

    with fish

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      <section className="bg-blue-50 dark:bg-slate-800" id="contact">
+      <section className="bg-gray-50 " id="contact">
     <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8 lg:py-20">
         <div className="mb-4">
             <div className="mb-6 max-w-3xl text-center sm:text-center md:mx-auto md:mb-12">
